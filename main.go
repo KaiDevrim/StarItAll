6a7600b019d79ac356c0fec936d08a001da3aced
Kai Devrim <kkgit@devrim.tech>
2021-04-19 20:50:09 -0700

    :construction: got the github access token and stored it in a buffer

--- main.go
+++ main.go
@@ -107,3 +107,37 @@ func getGithubData(accessToken string) string {
 	return string(respbody)
 }
 
+func getGithubAccessToken(code string) string {
+
+	clientID := getGithubClientID()
+	clientSecret := getGithubClientSecret()
+
+	requestBodyMap := map[string]string{"client_id": clientID, "client_secret": clientSecret, "code": code}
+	requestJSON, _ := json.Marshal(requestBodyMap)
+
+	req, reqerr := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(requestJSON))
+	if reqerr != nil {
+		log.Panic("Request creation failed")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	// Represents the response received from Github
+	type githubAccessTokenResponse struct {
+		AccessToken string `json:"access_token"`
+		TokenType   string `json:"token_type"`
+		Scope       string `json:"scope"`
+	}
+
+	var ghresp githubAccessTokenResponse
+	json.Unmarshal(respbody, &ghresp)
+
+	return ghresp.AccessToken
+}
