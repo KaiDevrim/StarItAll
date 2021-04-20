c872c74eb37032105496b72da59bcc3e9db72b99
Kai Devrim <kkgit@devrim.tech>
2021-04-19 20:51:03 -0700

    :sparkles: Finished the github oauth, need to test

--- main.go
+++ main.go
@@ -141,3 +141,23 @@ func getGithubAccessToken(code string) string {
 
 	return ghresp.AccessToken
 }
+
+func getGithubClientID() string {
+
+	githubClientID, exists := os.LookupEnv("CLIENT_ID")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientID
+}
+
+func getGithubClientSecret() string {
+
+	githubClientSecret, exists := os.LookupEnv("CLIENT_SECRET")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientSecret
+}
