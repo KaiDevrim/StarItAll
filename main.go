a921e2be93ec7038e011da56755de67f7e1b545b
Kai Devrim <kkgit@devrim.tech>
2021-04-19 20:49:17 -0700

    :construction: made getGithubData function

--- main.go
+++ main.go
@@ -88,3 +88,22 @@ func githubCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	loggedinHandler(w, r, githubData)
 }
 
+func getGithubData(accessToken string) string {
+	req, reqerr := http.NewRequest("GET", "https://api.github.com/user", nil)
+	if reqerr != nil {
+		log.Panic("API Request creation failed")
+	}
+
+	authorizationHeaderValue := fmt.Sprintf("token %s", accessToken)
+	req.Header.Set("Authorization", authorizationHeaderValue)
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	return string(respbody)
+}
+
